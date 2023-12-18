27803fc555d12b014f3c3d706bcf29a41222a402
mahmoodiftee <mahmoodiftee@gmail.com>
2023-12-19 03:53:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
+
+app.post('/signup', (req, res) => {
+    const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES(?)";
+    const values = [
+        req.body.username,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, data) => {
+        if (err) {
+            return res.json("error");
+        }
+        return res.json(data);
+    })
+})
+
+
+app.post('/login', (req, res) => {
+    const { identifier, password } = req.body;
+    const sql = "SELECT * FROM login WHERE `email` = ? OR `name` = ?";
+    db.query(sql, [identifier, identifier], (err, data) => {
+        if (err) {
+            return res.json("error");
+        }
+        if (data.length > 0) {
+            const user = data[0];
+            if (user.password === password) {
+                return res.json("success");
+            } else {
+                return res.json("incorrectPassword");
+            }
+        } else {
+            return res.json("incorrectIdentifier");
+        }
+    });
+});
+
+
+app.listen(8081, () => {
+    console.log("listening");;
+})
